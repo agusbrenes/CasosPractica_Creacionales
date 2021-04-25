1dc872493f1bbeb4eec997570f639862e228f496
agusbrenes <56280607+agusbrenes@users.noreply.github.com>
2021-04-24 19:02:06 -0600

    Main agregado

--- Problema6_Creacionales/src/problema6_creacionales/Problema6_Creacionales.java
+++ Problema6_Creacionales/src/problema6_creacionales/Problema6_Creacionales.java
@@ -6,6 +6,7 @@ public class Problema6_Creacionales {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        PiezaCreator factory = null;
         
         System.out.println("Cual forma desea para la Pieza?\n"
                 + "1. J\n"
@@ -19,16 +20,24 @@ public class Problema6_Creacionales {
         
         switch(num) {
             case 1:
+                factory = new PiezaJCreator();
             case 2:
+                factory = new PiezaLCreator();
             case 3:
+                factory = new PiezaTCreator();
             case 4:
+                factory = new PiezaZCreator();
             case 5:
+                factory = new PiezaSCreator();
             case 6:
+                factory = new PiezaECreator();
             case 7:
+                factory = new PiezaOCreator();
             default:
                 System.out.println("Numero introducido incorrecto... ://");
         }
-        //PiezaCreator factory = 
+        Pieza pieza = factory.createPieza();
+        System.out.println(pieza.toString());
     }
 
 }

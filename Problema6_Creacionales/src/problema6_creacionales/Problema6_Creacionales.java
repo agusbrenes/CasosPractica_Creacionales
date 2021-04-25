95022d9fc6dfa3d1f4e7e1dfd67e5b0b0a82deca
agusbrenes <56280607+agusbrenes@users.noreply.github.com>
2021-04-24 18:55:16 -0600

    Update Problema6_Creacionales.java

--- Problema6_Creacionales/src/problema6_creacionales/Problema6_Creacionales.java
+++ Problema6_Creacionales/src/problema6_creacionales/Problema6_Creacionales.java
@@ -1,9 +1,34 @@
 package problema6_creacionales;
 
+import java.util.Scanner;
+
 public class Problema6_Creacionales {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Cual forma desea para la Pieza?\n"
+                + "1. J\n"
+                + "2. L\n"
+                + "3. T\n"
+                + "4. Z\n"
+                + "5. S\n"
+                + "6. E\n"
+                + "7. O\n");
+        var num = input.nextInt();
+        
+        switch(num) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            default:
+                System.out.println("Numero introducido incorrecto... ://");
+        }
+        //PiezaCreator factory = 
     }
 
 }

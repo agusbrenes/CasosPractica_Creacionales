218552a3ab4d64f3eb873f4b9ccfea108256fe92
Chuz23 <48294718+Chuz23@users.noreply.github.com>
2021-04-25 20:26:26 -0600

    ConcreteBuilder y Main

--- Problema2_Creacionales/src/problema2_creacionales/Problema2_Creacionales.java
+++ Problema2_Creacionales/src/problema2_creacionales/Problema2_Creacionales.java
@@ -1,9 +1,28 @@
 package problema2_creacionales;
 
+import java.util.Scanner;
+
 public class Problema2_Creacionales {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Ventana window = new Ventana();
+        Builder builder;
+        String selec;
+        Director direc = new Director();
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Oscuro (1) \n Claro (2) \n Eliga un modo de color para la ventana :");
+        selec = scan.nextLine();
+        
+        if ("1".equals(selec))
+            builder = new VentanaOscuraBuilder();
+            
+        else 
+            builder = new VentanaClaraBuilder();
+        
+        direc.setVentanaBuilder(builder);
+        direc.construirVentana();
+        System.out.println(window.toString());
     }
 
 }

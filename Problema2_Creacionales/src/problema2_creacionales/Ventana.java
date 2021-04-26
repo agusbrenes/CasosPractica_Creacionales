f04899742a62c9ff1fbcb055770647daf1921056
agusbrenes <56280607+agusbrenes@users.noreply.github.com>
2021-04-25 18:36:58 -0600

    Create Ventana.java

---
+++ Problema2_Creacionales/src/problema2_creacionales/Ventana.java
@@ -0,0 +1,17 @@
+package problema2_creacionales;
+
+public class Ventana {
+    String componentsMode = "";
+
+    public Ventana() {
+    }
+    
+    public void setComponentsMode(String mode) {
+        componentsMode = mode;
+    }
+    
+    public String toString() {
+        return "Ventana con componentes en modo" + componentsMode + ".\n";
+    }
+
+}
